b490c20cb855b5ca3fb7fb098d038d20c8ee59d8
The Kaucher <muhammadkaucher018@gmail.com>
2025-03-24 22:27:39 +0600

    singup function for add input in database by api request:
    singup function for add input in database by api (thunder) request from body.json

--- larachara/app/Http/Controllers/authCpntroller.php
+++ larachara/app/Http/Controllers/authCpntroller.php
@@ -12,16 +12,16 @@ $user = User::all();
     return $user;
 
    }
-   // A singup function for add data in database by api request:
+   // A singup function for add input in database by api request:
 
    function singup(Request $req){
-      $input = $req->all(); //get api reqst from body/json
-      $input["password"]= bcrypt($input["password"]); //encrypt the input password by bcrypt
+      $input = $req->all(); //get api reqst from  body/json
+      $input["password"]= bcrypt($input["password"]); //encrypt the input passwords by bcrypt 
 
-      $user= User::create($input);//create a user in usertbale by input data
-      $success['token']=$user->createToken('Myapp')->plainTextToken; //create a token by $user
+      $user= User::create($input);//create a user in user table by input data
+      $success['token']=$user->createToken('Myapp')->plainTextToken; //create a token from $user
     
-      return ["result"=>$success]; //token number
+      return ["result"=>$success]; //return token number
 
     
    }
